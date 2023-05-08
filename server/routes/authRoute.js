0c3208193bad4f83bb6ada3254c8ce30efcb5ee1
tanishq0421 <tanishq.shukla.cd.che21@iitbhu.ac.in>
2023-05-08 22:44:29 +0530

    login functionality added

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -4,5 +4,6 @@ const auth = require('./../controllers/authController');
 const router = express.Router();
 
 router.post('/register', auth.register)
+router.post('/login', auth.login)
 
 module.exports = router;
